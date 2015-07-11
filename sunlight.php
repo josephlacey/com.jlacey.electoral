5e3bcd06926fd271bac1f8486dbb3de04192875e
Joseph Lacey <joseph@palantetech.coop>
2015-07-11 12:20:47 -0400

    Adding function to install custom data on install.  Adding navigation menu item.  Minor semantic cleanup.

--- sunlight.php
+++ sunlight.php
@@ -23,6 +23,26 @@ function sunlight_civicrm_xmlMenu(&$files) {
 }
 
 /**
+ * Implementation of hook_civicrm_navigationMenu
+ *
+ * @param $params array
+ */
+function sunlight_civicrm_navigationMenu(&$params) {
+  $path = "Administer/System Settings";
+  $item = array(
+    'label' => ts('Sunlight Foundation API', array('coop.palantetech.sunlight')),
+    'name' => 'Sunlight Foundation API',
+    'url' => 'civicrm/admin/setting/sunlight',
+    'permission' => 'administer CiviCRM',
+    'operator' => '',
+    'separator' => '',
+    'active' => 1,
+  );
+
+  $navigation = _sunlight_civix_insert_navigation_menu($params, $path, $item);
+}
+
+/**
  * Implementation of hook_civicrm_install
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -48,6 +68,7 @@ function sunlight_civicrm_install() {
       'run_frequency' => 'Daily',
       'api_entity'    => 'Congress',
       'api_action'    => 'districts',
+      'parameters'    => 'limit=1',
       'is_active'     => 0,
     );
     $congress_districts_job = civicrm_api3('job', 'create', $congress_districts_job_params);
@@ -72,6 +93,7 @@ function sunlight_civicrm_install() {
       'run_frequency' => 'Daily',
       'api_entity'    => 'OpenStates',
       'api_action'    => 'districts',
+      'parameters'    => 'limit=1',
       'is_active'     => 0,
     );
     $openstates_districts_job = civicrm_api3('job', 'create', $openstates_districts_job_params);
@@ -84,6 +106,7 @@ function sunlight_civicrm_install() {
       'run_frequency' => 'Daily',
       'api_entity'    => 'NyTimes',
       'api_action'    => 'districts',
+      'parameters'    => 'limit=1',
       'is_active'     => 0,
     );
     $nytimes_districts_job = civicrm_api3('job', 'create', $nytimes_districts_job_params);
@@ -98,23 +121,23 @@ function sunlight_civicrm_install() {
  */
 function sunlight_civicrm_uninstall() {
   //Deletes Sunlight Foundation Jobs
-  $jobs = civicrm_api3('Job', 'get', array(
+  $sunlight_jobs = civicrm_api3('Job', 'get', array(
     'return' => "id",
     'name' => array('LIKE' => "Sunlight Foundation%"),
   ));
 
-  foreach ($jobs['values'] as $job) {
-    $job_delete = civicrm_api3('job', 'delete', array('id' => $job['id'] ));
+  foreach ($sunlight_jobs['values'] as $sunlight_job) {
+    $sunlight_job_delete = civicrm_api3('job', 'delete', array('id' => $sunlight_job['id'] ));
   }
 
   //Deletes Sunlight Foundation Jobs
-  $jobs = civicrm_api3('Job', 'get', array(
+  $ny_times_jobs = civicrm_api3('Job', 'get', array(
     'return' => "id",
     'name' => array('LIKE' => "NY Times Districts API"),
   ));
 
-  foreach ($jobs['values'] as $job) {
-    $job_delete = civicrm_api3('job', 'delete', array('id' => $job['id'] ));
+  foreach ($ny_times_jobs['values'] as $ny_times_job) {
+    $ny_times_job_delete = civicrm_api3('job', 'delete', array('id' => $ny_times_job['id'] ));
   }
 
   return _sunlight_civix_civicrm_uninstall();
@@ -126,7 +149,8 @@ function sunlight_civicrm_uninstall() {
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function sunlight_civicrm_enable() {
-  _sunlight_civix_civicrm_enable();
+  sunlight_create_custom_fields();
+  return _sunlight_civix_civicrm_enable();
 }
 
 /**
@@ -186,3 +210,141 @@ function sunlight_civicrm_caseTypes(&$caseTypes) {
 function sunlight_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
   _sunlight_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
+
+/**
+ * Create the custom fields used to record subject and body
+ *
+ */
+function sunlight_create_custom_fields() {
+  //Check if Representation Details custom data group already exists
+  $rd_group = civicrm_api3('CustomGroup', 'get', array( 'title' => "Representative Details", ));
+
+  if ($rd_group['count'] == 0) {
+    //If not, create it
+    $rd_group_create = civicrm_api3('CustomGroup', 'create', array(
+      'sequential' => 1,
+      'title' => "Representative Details",
+      'extends' => "Contact",
+      'name' => "Representative_Details",
+      'style' => "Tab with table",
+      'is_multiple' => 1,
+    ));
+
+    //Create Level Option Group and Values
+    $rd_level_og = civicrm_api3('OptionGroup', 'create', array(
+      'name' => "sunlight_level_options",
+      'title' => "Level",
+      'is_active' => 1,
+    ));
+    $rd_level_id = $rd_level_og['id'];
+    $rd_level_congress = civicrm_api3('OptionValue', 'create', array(
+      'option_group_id' => "sunlight_level_options",
+      'label' => "Federal",
+      'value' => "congress",
+      'name' => "Federal",
+      'weight' => 1,
+      'is_active' => 1,
+    ));
+    $rd_level_openstates = civicrm_api3('OptionValue', 'create', array(
+      'option_group_id' => "sunlight_level_options",
+      'label' => "State/Province",
+      'value' => "openstates",
+      'name' => "State/Province",
+      'weight' => 2,
+      'is_active' => 1,
+    ));
+    $rd_level_city = civicrm_api3('OptionValue', 'create', array(
+      'option_group_id' => "sunlight_level_options",
+      'label' => "City",
+      'value' => "nytimes",
+      'name' => "City",
+      'weight' => 3,
+      'is_active' => 1,
+    ));
+    
+    //Create Chamber Option Group and Values
+    $rd_chamber_og = civicrm_api3('OptionGroup', 'create', array(
+      'name' => "sunlight_chamber_options",
+      'title' => "Chamber",
+      'is_active' => 1,
+    ));
+    $rd_chamber_id = $rd_chamber_og['id'];
+    $rd_chamber_upper = civicrm_api3('OptionValue', 'create', array(
+      'option_group_id' => "sunlight_chamber_options",
+      'label' => "Upper",
+      'value' => "upper",
+      'name' => "Upper",
+      'weight' => 1,
+      'is_active' => 1,
+    ));
+    $rd_chamber_lower = civicrm_api3('OptionValue', 'create', array(
+      'option_group_id' => "sunlight_chamber_options",
+      'label' => "Lower",
+      'value' => "lower",
+      'name' => "Lower",
+      'weight' => 2,
+      'is_active' => 1,
+    ));
+    
+    //Create Representative Details Fields
+    $rd_level_field = civicrm_api3('CustomField', 'create', array(
+      'custom_group_id' => "Representative_Details",
+      'label' => "Level",
+      'name' => "sunlight_level",
+      'data_type' => "String",
+      'html_type' => "Select",
+      'is_searchable' => 1,
+      'weight' => 1,
+      'is_active' => 1,
+      'option_group_id' => $rd_level_id,
+      'in_selector' => 1,
+    ));
+    $rd_states_provinces_field = civicrm_api3('CustomField', 'create', array(
+      'custom_group_id' => "Representative_Details",
+      'label' => "States/Provinces",
+      'name' => "sunlight_states_provinces",
+      'data_type' => "StateProvince",
+      'html_type' => "Select State/Province",
+      'is_searchable' => 1,
+      'weight' => 2,
+      'is_active' => 1,
+      'in_selector' => 1,
+    ));
+    $rd_chamber_field = civicrm_api3('CustomField', 'create', array(
+      'custom_group_id' => "Representative_Details",
+      'label' => "Chamber",
+      'name' => "sunlight_chamber",
+      'data_type' => "String",
+      'html_type' => "Select",
+      'is_searchable' => 1,
+      'weight' => 3,
+      'is_active' => 1,
+      'option_group_id' => $rd_chamber_id,
+      'in_selector' => 1,
+    ));
+    $rd_district_field = civicrm_api3('CustomField', 'create', array(
+      'custom_group_id' => "Representative_Details",
+      'label' => "District",
+      'name' => "sunlight_district",
+      'data_type' => "Integer",
+      'html_type' => "Text",
+      'is_searchable' => 1,
+      'weight' => 4,
+      'is_active' => 1,
+      'in_selector' => 1,
+    ));
+    $rd_in_office_field = civicrm_api3('CustomField', 'create', array(
+      'sequential' => 1,
+      'custom_group_id' => "Representative_Details",
+      'label' => "In Office?",
+      'name' => "sunlight_in_office",
+      'data_type' => "Boolean",
+      'html_type' => "Radio",
+      'is_searchable' => 1,
+      'weight' => 5,
+      'is_active' => 1,
+      'in_selector' => 1,
+    ));
+  }
+}
+
